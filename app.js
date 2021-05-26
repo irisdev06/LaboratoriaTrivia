4d305b6b7ed30092f2d0532962a18a23b812dfa1
Iris Val <69370348+irisdev06@users.noreply.github.com>
2021-05-26 15:01:39 -0500

    Create app.js

---
+++ app.js
@@ -0,0 +1,6 @@
+function guardarNombre() {
+    var nombre = prompt('¿Cuál es tu nombre?')
+    alert("Hola, "+ nombre)
+} //Creamos la función para que guarde el nombre y no se abra el prompt al cargar la página.
+
+ScrollReveal().reveal('.bienvenida');
