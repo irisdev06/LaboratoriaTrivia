2d918d4e1d6476b814b722115a20564b3ac2383d
irisdev06 <ivgb_@outlook.com>
2021-05-27 17:09:28 -0500

    Condicionales

--- app.js
+++ app.js
@@ -12,4 +12,35 @@ function guardarNombre() {
 
 
 
+//Hicimos una condicional preguntando donde estamos y si nos devuelve el valor igual al valor triviahistoria
+if (window.location.pathname == "/triviahistoria.html") {
+    let preguntas = document.getElementById('preguntas')
+    let numPregunta = 1;
+    let respuestasCorrectas = 0;
 
+    preguntas.innerHTML = `<button class="comenzar" onClick="pregunta(${numPregunta})">Empieza la trivia de Historia</button>`
+
+    function pregunta(pregunta) {
+        if (pregunta == 1) {
+            preguntas.innerHTML = `
+<h1>¿Cómo se llamaba el comandante de 80 soldados en el ejercito romano antiguo?</h1>
+<label><input type="radio" name="respuesta">Centurion</label><br>
+<label><input type="radio" name="respuesta">Leonidas</label><br>
+<label><input type="radio" name="respuesta">Bonaparte</label><br>
+<button class="enviar" onClick="enviar(${numPregunta})">Enviar</button>`
+        } else if (pregunta == 2) {
+            preguntas.innerHTML = `
+<h1>Qué mes lleva el nombre de un emperador romano?</h1>
+<label><input type="radio" name="respuesta">Enero</label><br>
+<label><input type="radio" name="respuesta">Febrero</label><br>
+<label><input type="radio" name="respuesta">Agosto</label><br>
+<button class="enviar" onClick="enviar(${numPregunta})">Enviar</button>`
+} else if (pregunta == 3) {
+    preguntas.innerHTML = `
+<h1>"¿Qué dama Austriaca se convirtió en reina de francia en 1774?" en Colombia?</h1>
+<label><input type="radio" name="respuesta">Maria Antonieta</label><br>
+<label><input type="radio" name="respuesta">Juana de arco</label><br>
+<label><input type="radio" name="respuesta">Emma de provenza</label><br>
+<button class="enviar" onClick="enviar(${numPregunta})">Enviar</button>`
+}
+    
